56d68add842aa29a183dfa31daef4eb5e17ca851
Gonzalo <gonzabarreiro7@gmail.com>
2022-10-11 07:02:03 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+// Definición de persona
+
+/* let nombre = 'Gonzalo'
+let edad = 26
+let luckynumber = '17'
+let tvshow = [{pelicula: 'Avengers'}, {serie: 'Game of Thrones & House of the Dragon'}] */
+
+
+let nombre = 'Pepe'
+let edad = 25
+let precio = 99.90
+let series = ['Dark', 'Mr. Robot', 'Castlevania']
+let peliculas = [{
+    nombre: 'minions',
+    anio: '2008',
+    protagonistas: ['Gru', 'Bob', 'Kevin']
+}]
+
+edad = edad + 1
+
+series.push('Breaking Bad')
+series.push(' Bad')
+peliculas.push(' Bad')
+
+console.log(nombre);
+console.log(edad);
+console.log(precio);
+console.log(series);
+console.log(peliculas);
