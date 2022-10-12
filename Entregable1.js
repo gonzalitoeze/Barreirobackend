8692c7a16162ebeba78e6a6b5c801830ec728b76
gonzalitoeze <103007269+gonzalitoeze@users.noreply.github.com>
2022-10-13 00:12:33 +0200

    Entregable1.js

--- Entregable1.js
+++ Entregable1.js
@@ -2,7 +2,16 @@ class Usuario {
     constructor (nombre, apellido, libros, mascotas) {
         this.nombre = nombre ;
         this.apellido = apellido ;
-        this.libros = [libros] ;
+        this.libros = [
+            {
+                nombre: 'Harry Potter', 
+                autor:' J. K. Rowling'
+            },
+            {
+                nombre: 'La Isla Misteriosa',
+                autor: ' Julio Verne'
+            }
+        ];
         this.mascotas = [mascotas] ;
     }
     
@@ -12,7 +21,7 @@ class Usuario {
 
 
     retornarMensaje () {
-        return `Buenas! Soy ${this.nombre} ${this.apellido}, tengo tres mascotas( ${this.mascotas}) y mis libros preferidos son ${this.libros}`;
+        return `Buenas! Soy ${this.nombre} ${this.apellido}, tengo tres mascotas( ${this.mascotas}) y mis libros preferidos son ${this.libros.map(item => item.nombre)} de los autores ${this.libros.map(item => item.autor)}`;
     } 
 
     
