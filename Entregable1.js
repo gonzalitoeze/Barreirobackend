cd88633bac1c9c79ab5c02009b2be0f85eb4e5ca
gonzalitoeze <103007269+gonzalitoeze@users.noreply.github.com>
2022-10-17 13:52:39 +0200

    Entregable1

--- Entregable1.js
+++ Entregable1.js
@@ -26,22 +26,25 @@ class Usuario {
     addMascota(mascota) {
         this.mascotas.push(mascota);
     }
+
+    getBookNames(libros) {
+        this.libros.lenght(libros)
+    }
 }
 
 
 
 const countMascotas = ['Popis', 'Spyke', 'Roma']
 
-const User = new Usuario ('Gonzalo', 'Barreiro', [{nombre: 'La Isla Misteriosa', autor: 'Julio Verne'}/* , {nombre: 'Harry Potter', autor: 'J. K. Rowling'} */], ['Popis', 'Spyke' ]);
+const User = new Usuario ('Gonzalo', 'Barreiro', [{nombre: 'La Isla Misteriosa', autor: 'Julio Verne'}], ['Popis', 'Spyke' ]);
 console.log(User);
 // console.log(User.retornarMensaje())
-console.log(User.libros);
 User.addBook({nombre: 'Harry Potter', autor: 'J. K. Rowling'})
 console.log(User.libros)
-console.log(User.mascotas);
 User.addMascota('Roma');
 console.log(User.mascotas);
 console.log(countMascotas.length);
+console.log(User.getBookNames.map);
 
 // console.log(User.getFullName())
 
