21ba89a90248d0d0143c4ba3c8ccdcf17105de0a
gonzalitoeze <103007269+gonzalitoeze@users.noreply.github.com>
2022-10-26 09:39:20 +0200

    Entregable3

--- main.js
+++ main.js
@@ -1,39 +1,50 @@
-const express = require('express');
+const express = require("express");
+const Contenedor = require("./Contenedor");
+const productos = new Contenedor("./productos.json");
 const app = express();
-const Contenedor = require('./Contenedor')
-const productos = require('./productos.json')
-const puerto = 8080;
+const PORT = 8080;
 
-app.get('/', (req, res) => {
+const server = app.listen(PORT, () => {
+    console.log(
+        `Servidor http escuchando en el puerto ${server.address().port}`
+    );
+});
+server.on("error", error => console.log(`Error en servidor: ${error.message}`));
+
+app.get("/", (req, res) => {
     try {
-        res.end('<h1 style="color:green">Bienvenidos al servidor express</h1> </hr> <h3>Desafío entregable n°3</h3>');
-    } catch (err) {
-        console.log(`Error al cargar ${err}`);
+        res.send(
+            '<div style="color: green; text-align: center"><h1>Bienvenidos al servidor express</h1></div> </hr> <h3 style="color: black; text-align:center">Desafío entregable n°3</h3>'
+        );
+    } catch (error) {
+        console.log(`Error cargando el proyecto: ${error}`);
     }
 });
 
-app.get('/productos', (req, res) => {
-    const run = async () => {
+app.get("/productos", (req, res) => {
+    const ejecutar = async () => {
         try {
             const arrayProductos = await productos.getAll();
             let card = ``;
             arrayProductos.map(
-                item => 
-                (card += `<div style="background-color: black; color: white; text-align: center; height: auto; width: 300px"><h2>Nombre: ${item.title}</h2><h3>Precio: ${item.price}</h3><img style="margin-bottom: 10px" height="250px" src="${item.thumbnail}"></div>`)
-            )
-            res.send(`<h1 style="text-align: center">Todos los productos:</h1><section style="display: flex; justify-content: space-around">${card}</section>`)
-        } catch (err) {
-                console.log(`Error en carga de productos ${err}`);
-            } 
+                item =>
+                    (card += `<div style="background-color: white; color: white; text-align: center; height: auto; width: 300px"><h2>Nombre: ${item.title}</h2><h3>Precio: ${item.price}</h3><img style="margin-bottom: 10px" height="250px" src="${item.thumbnail}"></div>`)
+            );
+            res.send(
+                `<h1 style="text-align: center">Todos los productos:</h1><section style="display: flex; justify-content: space-around">${card}</section>`
+            );
+        } catch (error) {
+            console.log(`Error obteniendo todos los productos: ${error}`);
+        }
     };
-    run();
-})
+    ejecutar();
+});
 
-app.get('/prodRANDOM', (req, res) => {
-    const run = async () => {
+app.get("/random", (req, res) => {
+    const ejecutar = async () => {
         try {
             const arrayProductos = await productos.getAll();
-            let numberRandom = Math.floor(Math.random() * productos.length)
+            let numero = Math.floor(Math.random() * arrayProductos.length);
             const producto = await productos.getById(numero + 1);
             let card = `<div style="background-color: black; color: white; text-align: center; height: auto; width: 300px"><h2>Nombre: ${producto[0].title}</h2> <h3>Precio: ${producto[0].price}</h3> <img style="margin-bottom: 10px"height="250px" src="${producto[0].thumbnail}"></div>`;
             res.send(
@@ -43,11 +54,62 @@ app.get('/prodRANDOM', (req, res) => {
             console.log(`Error obteniendo un producto al azar: ${error}`);
         }
     };
-    run();
-})
-
-const conexionServidor = app.listen(puerto, () => {
-    console.log(`Aplicación escuchando en el puerto ${conexionServidor.address().port}`);
+    ejecutar();
 });
 
-conexionServidor.on('Error', error => console.log(`Ha ocurido un error: ${error}`));
+
+
+
+// const express = require('express');
+// const app = express();
+// const Contenedor = require('./Contenedor')
+// const productos = require('./productos.json')
+// const puerto = 8080;
+
+// app.get('/', (req, res) => {
+//     try {
+//         res.end('<h1 style="color:green">Bienvenidos al servidor express</h1> </hr> <h3>Desafío entregable n°3</h3>');
+//     } catch (err) {
+//         console.log(`Error al cargar ${err}`);
+//     }
+// });
+
+// app.get('/productos', (req, res) => {
+//     const run = async () => {
+//         try {
+//             const arrayProductos = await productos.getAll();
+//             let card = ``;
+//             arrayProductos.map(
+//                 item => 
+//                 (card += `<div style="background-color: black; color: white; text-align: center; height: auto; width: 300px"><h2>Nombre: ${item.title}</h2><h3>Precio: ${item.price}</h3><img style="margin-bottom: 10px" height="250px" src="${item.thumbnail}"></div>`)
+//             )
+//             res.send(`<h1 style="text-align: center">Todos los productos:</h1><section style="display: flex; justify-content: space-around">${card}</section>`)
+//         } catch (err) {
+//                 console.log(`Error en carga de productos ${err}`);
+//             } 
+//     };
+//     run();
+// })
+
+// app.get('/prodRANDOM', (req, res) => {
+//     const run = async () => {
+//         try {
+//             const arrayProductos = await productos.getAll();
+//             let numberRandom = Math.floor(Math.random() * productos.length)
+//             const producto = await productos.getById(numero + 1);
+//             let card = `<div style="background-color: black; color: white; text-align: center; height: auto; width: 300px"><h2>Nombre: ${producto[0].title}</h2> <h3>Precio: ${producto[0].price}</h3> <img style="margin-bottom: 10px"height="250px" src="${producto[0].thumbnail}"></div>`;
+//             res.send(
+//                 `<h1 style="text-align: center">Producto al azar:</h1><section style="display: flex; justify-content: space-around">${card}</section>`
+//             );
+//         } catch (error) {
+//             console.log(`Error obteniendo un producto al azar: ${error}`);
+//         }
+//     };
+//     run();
+// })
+
+// const conexionServidor = app.listen(puerto, () => {
+//     console.log(`Aplicación escuchando en el puerto ${conexionServidor.address().port}`);
+// });
+
+// conexionServidor.on('Error', error => console.log(`Ha ocurido un error: ${error}`));
